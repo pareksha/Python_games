dea2f3ee7e15ddc40a0af5644579fdf67cd02beb
pareksha <pareksha.manchanda@gmail.com>
2017-11-07 22:54:44 +0530

    guessing game

---
+++ Guessing_game.py
@@ -0,0 +1,21 @@
+'''Guess the number game'''
+
+import random
+right_guess = random.randint(1,2)
+flag = False
+print("Guess a number between 1 and 10")
+
+
+for i in range(0,3):
+    if(flag == False):
+        input_number = int(input())
+        if(input_number == right_guess):
+            print("You guessed right!")
+            print("You won..!!")
+            flag = True
+        else:
+            print("Try Again!")
+            print("You have {} turn(s) left..!!".format(2-i))
+
+if(flag == False):
+    print("You lost..!\nBetter luck next time...!!")
