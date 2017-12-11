b2fe111669c21fa32609d3126c03b1456ff2c6f3
pareksha <pareksha.manchanda@gmail.com>
2017-12-11 16:31:34 +0530

    Repeated play feature added

--- mouse_hunter.py
+++ mouse_hunter.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 
 def initialiseBoard():
@@ -17,7 +18,12 @@ def printBoard(list_):
     for column_list in list_:
         print(str(i).ljust(3), end=' ')
         for column in column_list:
-            print(column, end='  ')
+            if len(column) == 1:
+                print(column, end='  ')
+            elif len(column) == 2:
+                print(column, end=' ')
+            else:
+                print(column, end='')
         print('')
         i += 1
     return
@@ -49,19 +55,19 @@ def getUserGuess():
 
 
 def howFar(user_row, user_col, mouse_row, mouse_col):
-    return abs(user_row - mouse_row + user_col - mouse_col)
+    return abs(user_row - mouse_row) + abs(user_col - mouse_col)
 
 
 def update_grid(user_row, user_column, distance):
     if distance == 0:
         grid_list[user_row - 1][user_column - 1] = 'X'
     else:
-        grid_list[user_row - 1][user_column - 1] = distance
+        grid_list[user_row - 1][user_column - 1] = str(distance)
     return
 
 
 def main():
-    print("Let's Play")
+    print("\nLet's Play")
     x = 1
     while x:
         no_of_lives = int(input('How many lives do you want? (Min 1, Max 10) : '))
@@ -84,6 +90,7 @@ def main():
         distance = howFar(user_guess_row, user_guess_col,
                           mouse_row, mouse_column)
         update_grid(user_guess_row, user_guess_col, distance)
+        # print(mouse_row, mouse_column)
         if distance == 0:
             print('\n')
             printBoard(list_of_grid_values)
@@ -93,9 +100,25 @@ def main():
         n -= 1
         print('\n')
 
-    print(mouse_row, mouse_column)
+    if flag:
+        printBoard(list_of_grid_values)
+        print('\nYou ran out of lives !\n')
+
+    return
+
     # print(user_guess_row, user_guess_col)
 
 
 if __name__ == '__main__':
-    main()
+    while 1:
+        main()
+        flag = True
+        while flag:
+            ans = input('Want to play again ? (Yes/No) -> ')
+            if ans == 'Yes':
+                flag = False
+            elif ans == 'No':
+                print('Bye, see you next time !')
+                sys.exit()
+            else:
+                print("\nWrong Input, let's try again !\n")
