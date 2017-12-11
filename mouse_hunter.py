a180916b4f30d0f6642b64b34c2b1c567134fe5f
pareksha <pareksha.manchanda@gmail.com>
2017-12-11 19:00:09 +0530

    Game play by computer added

--- mouse_hunter.py
+++ mouse_hunter.py
@@ -41,8 +41,13 @@ def placeMouse():
     """Place the mouse at random position on the grid.
         Return the row and column number of the mouse
     """
-    mouse_row_number = random.randint(1, len(grid_list))
-    mouse_column_number = random.randint(1, len(grid_list))
+    if player == 'User':
+        mouse_row_number = random.randint(1, len(grid_list))
+        mouse_column_number = random.randint(1, len(grid_list))
+    else:
+        print('\nInitialise row and column for the mouse : ')
+        mouse_row_number = int(input('Row Number: '))
+        mouse_column_number = int(input('Column Number: '))
     return mouse_row_number, mouse_column_number
 
 
@@ -86,18 +91,31 @@ def update_grid(user_row, user_column, distance):
     return
 
 
-def main():
-    """Game Play.
-    """
-    print("\nLet's Play")
+def input_lives():
     x = 1
     while x:
-        no_of_lives = int(input('How many lives do you want? (Min 1, Max 10) : '))
+        # option to change the number of lives added
+        if player == 'User':
+            no_of_lives = int(input('How many lives do you want? '
+                                    '(Min 1, Max 10) : '))
+        else:
+            no_of_lives = int(input('How many lives do you want to give to me? '
+                                    '(Min 1, Max 10) : '))
         if no_of_lives in range(1, 11):
             x = 0
         else:
-            print('Lives must be between 1 and 5. Please try again !')
+            print('Lives must be between 1 and 10. Please try again !')
+    return no_of_lives
 
+
+def user_play():
+    """Game Play by user.
+    """
+    global player
+    player = 'User'
+    print("\nLet's Play")
+
+    no_of_lives = input_lives()
     list_of_grid_values = initialiseBoard()
     mouse_row, mouse_column = placeMouse()
     print()
@@ -128,11 +146,74 @@ def main():
     return
 
 
+def computer_play():
+    """Game Play by computer
+    """
+    global player
+    player = 'Computer'
+    print("\nSee my play")
+
+    no_of_lives = input_lives()
+    list_of_grid_values = initialiseBoard()
+    computer_guess_list = [(x,y) for x in range(1,len(list_of_grid_values) + 1)
+                           for y in range(1,len(list_of_grid_values) + 1)]
+    random.shuffle(computer_guess_list)
+    mouse_row, mouse_column = placeMouse()
+    print()
+    n = no_of_lives
+    flag = True
+    dist = random.randint(1, len(list_of_grid_values))
+    while flag and n:
+        printBoard(list_of_grid_values)
+        print('\n')
+        print('Guess {} out of {}...'.format(no_of_lives - n + 1, no_of_lives))
+        if n != no_of_lives:
+            previous_row_guess = computer_guess_row
+            previous_column_guess = computer_guess_column
+
+        for tuple_ in computer_guess_list:
+            computer_guess_row, computer_guess_column = tuple_
+            if n != no_of_lives:
+                if abs(computer_guess_row - previous_row_guess) + \
+                        abs(computer_guess_column - previous_column_guess) == distance\
+                        and list_of_grid_values[computer_guess_row - 1] \
+                        [computer_guess_column - 1] == 'O':
+                    break
+            else:
+                break
+        distance = howFar(computer_guess_row, computer_guess_column,
+                          mouse_row, mouse_column)
+        update_grid(computer_guess_row, computer_guess_column, distance)
+        if distance == 0:
+            print('\n')
+            printBoard(list_of_grid_values)
+            print('\nYeah ! I caught the mouse in {} moves.'
+                  .format(no_of_lives - n + 1))
+            flag = False
+        n -= 1
+        print()
+
+    if flag:
+        printBoard(list_of_grid_values)
+        print('\nSorry, I ran out of lives !\n')
+
+    return
+
+
 if __name__ == '__main__':
     while 1:
-        main()
+        play = input('Want the computer to play and illustrate the game ? '
+                     '(Yes/No) -> ')
+        if play == 'Yes':
+            computer_play()
+        elif play == 'No':
+            user_play()
+        else:
+            print('Wrong Input. Try Again !')
+            continue
         flag = True
         while flag:
+            # option to play again at the end
             ans = input('Want to play again ? (Yes/No) -> ')
             if ans == 'Yes':
                 flag = False
